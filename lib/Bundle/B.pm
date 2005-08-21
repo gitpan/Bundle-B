9a3af97b7439dd92424e7d764a0c943062566463
Philip M. Gollucci <pgollucci@p6m7g8.com>
2005-08-21 08:56:08 +0000

    Import of PGOLLUCCI/Bundle-B-0.01_01 from CPAN.
    gitpan-cpan-distribution: Bundle-B
    gitpan-cpan-version:      0.01_01
    gitpan-cpan-path:         PGOLLUCCI/Bundle-B-0.01_01.tar.gz
    gitpan-cpan-author:       PGOLLUCCI
    gitpan-cpan-maturity:     developer

---
+++ lib/Bundle/B.pm
@@ -0,0 +1,46 @@
+package Bundle::B;
+
+$VERSION = '0.01_01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::B -- ALL the PERL B::* modules
+
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::B'>
+
+=head1 CONTENTS
+
+Asmdata
+Assembler
+Bblock
+Bytecode
+C
+CC
+Concise
+Debug
+Deparse
+Disassembler
+Fathom
+Graph
+LexInfo
+Lint
+Showlex
+Size
+Stackobj
+Stash
+Terse
+TerseSize
+Xref
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Philip M. Gollucci <pgolucci@p6m7g8.com>
